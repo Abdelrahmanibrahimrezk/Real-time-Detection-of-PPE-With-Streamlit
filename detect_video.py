4fff3e010d702534ec24f976b3fbe61f23a467a2
Abdelrahman Mahmoud <89320483+Abdelrahmanibrahimrezk@users.noreply.github.com>
2022-11-21 14:43:00 +0200

    Update detect_video.py

--- detect_video.py
+++ detect_video.py
@@ -80,8 +80,8 @@ def detect_video(vid):
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
         
-        #out.write(result)
-        return result
+        out.write(result)
+        
         if cv2.waitKey(1) & 0xFF == ord('q'): break
     cv2.destroyAllWindows()
 #     return "./detections/results1.mp4"
